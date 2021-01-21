3bba66b4d35a1ebb3dc8e950356d2538816fc36e
jcgb1996 <35897807+jcgb1996@users.noreply.github.com>
2021-01-20 20:09:46 -0500

    fin seccion 3

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+//app de express
+const app = express();
+
+//node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./socket/socket')
+
+
+
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+
+
+server.listen(process.env.PORT, (err)=>{
+        if(err)  {
+            throw new Error(error);
+        }
+        console.log('servidor corriendo en el puerto', process.env.PORT);
+});
